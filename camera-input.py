2e1d4051fce59fa5154939529f37abbb76566f95
Matt Brucker <mpbrucker@gmail.com>
2018-12-15 16:22:33 -0500

    Integrating Uploadcare with our scripts.

--- camera-input.py
+++ camera-input.py
@@ -2,12 +2,17 @@ import cv2 as cv
 import numpy as np
 import subprocess as sp
 from repo_update import update_yaml_link
+from imageupload import UC_PUBLIC_KEY, UC_PRIVATE_KEY, init_uc, upload_image
+
+init_uc(UC_PUBLIC_KEY, UC_PRIVATE_KEY)
+cap = cv.VideoCapture(0)
+
 
 def nothing(x):
     pass
 
 def process_camera_input():
-    pipe = sp.Popen("gphoto2 --capture-movie --stdout | ffmpeg -i pipe:0 -pix_fmt bgr24 -vcodec rawvideo -an -sn -f image2pipe -", stdout = sp.PIPE, bufsize=10**8, shell=True)
+    # pipe = sp.Popen("gphoto2 --capture-movie --stdout | ffmpeg -i pipe:0 -pix_fmt bgr24 -vcodec rawvideo -an -sn -f image2pipe -", stdout = sp.PIPE, bufsize=10**8, shell=True)
 
     cv.namedWindow('frame')
     # cv.createTrackbar('thresh', 'frame', 117, 255, nothing)
@@ -16,11 +21,12 @@ def process_camera_input():
 
 
     while(True):
-        raw_image = pipe.stdout.read(960*640*3)
+        # raw_image = pipe.stdout.read(960*640*3)
+        ret, frame = cap.read()
         # transform the byte read into a numpy array
-        frame =  np.fromstring(raw_image, dtype='uint8')
+        # frame =  np.fromstring(raw_image, dtype='uint8')
         if len(frame) != 0:
-                frame = frame.reshape((640,960,3))          # Notice how height is specified first and then width
+                # frame = frame.reshape((640,960,3))          # Notice how height is specified first and then width
                 if frame is not None:
                     
 
@@ -51,12 +57,16 @@ def process_camera_input():
                 # save image 
                 elif key & 0xFF == ord('e'):
                     retval, output_image = cv.imencode('.jpg', frame)
+                    img_out = upload_image(output_image)
+                    img_url = img_out.cdn_url
+                    print(img_url)
                     print(type(output_image))
+                    update_yaml_link(img_url)
                     # cv.imwrite('test.jpg', frame)
                     # print(output_image)
                     break
 
-                pipe.stdout.flush()
+                # pipe.stdout.flush()
 
     cv.destroyAllWindows()
 
