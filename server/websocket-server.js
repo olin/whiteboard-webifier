1622a31e7afed0cdaf101355d5fd9b59118a7862
Matt Brucker <mpbrucker@gmail.com>
2019-01-05 00:02:04 -0500

    Adding auth message.

--- server/websocket-server.js
+++ server/websocket-server.js
@@ -9,22 +9,22 @@ module.exports = class websocketServer {
         
         // Setup websocket listening functions
         this.io.on('connection', function(socket) {
-            socket.on('point-vals', function(data) {
-                let authString = socket.handshake.headers.authorization;
+            let authString = socket.handshake.headers.authorization;
 
-                // An auth header must exist
-                if (authString != null) {
-                    let authStringDecoded = Buffer.from(authString.slice(6), 'base64').toString('ascii')
+            // An auth header must exist
+            if (authString != null) {
+                let authStringDecoded = Buffer.from(authString.slice(6), 'base64').toString('ascii');
+                let expectedAuth = process.env.ID + ':' + process.env.PASSWORD;
 
-                    let expectedAuth = process.env.ID + ':' + process.env.PASSWORD
-
-                    if (authStringDecoded === expectedAuth) {
+                // We only trust point updates from authenticated clients
+                if (authStringDecoded === expectedAuth) {
+                    console.log('Client ' + socket.id + ' Authenticated.');
+                    socket.on('point-vals', function(data) {
                         socket.broadcast.emit('points', data);
-                    } else {
-                        socket.disconnect()
-                    }
-                }
-            })
+                    })
+                } 
+            }
+    
             console.log('Client connected.');
         });
     
